ae15002eb72a592a14759ec9828971b45c994eaf
r4to <r4topunk.eth@gmail.com>
2025-03-25 17:46:23 -0300

    refactor: simplify layout in Screen component for better readability
    Removed unnecessary nested View components to streamline the layout
    of the Screen component, enhancing code clarity and maintainability.

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,8 @@ import { Feed } from '~/components/Feed';
 export default function Screen() {
   return (
     <ScrollView className='flex-1 bg-background'>
-      <View className='justify-center items-center gap-5 p-6'>
-        <View className='w-full max-w-sm'>
-          <Feed />
-        </View>
+      <View className='w-full p-4'>
+        <Feed />
       </View>
     </ScrollView>
   );
