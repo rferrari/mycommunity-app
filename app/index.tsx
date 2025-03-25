da736fb50bbe1ee8457e02776a8a2ab7e137770f
r4to <r4topunk.eth@gmail.com>
2025-03-25 15:04:43 -0300

    style: adjust padding in Card component for consistency

--- app/index.tsx
+++ app/index.tsx
@@ -30,9 +30,9 @@ export default function Screen() {
 
   return (
     <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-      <Card className='w-full max-w-sm p-6 rounded-2xl'>
+      <Card className='w-full max-w-sm rounded-2xl'>
         <CardHeader>
-          <CardTitle className='pb-2 text-center'>API Feed Data</CardTitle>
+          <CardTitle className='py-2 text-center'>API Feed Data</CardTitle>
         </CardHeader>
         <CardContent className='flex-col gap-4'>
           {errorMessage ? (
